0541c9ba5cb26ec578021da074b20556483e8210
Jorge Gomez <jgomez@picap.co>
2021-03-01 15:53:22 -0500

    Pantalla Inicio

---
+++ app/src/main/java/com/example/proyectodonpepe/pantallaInicio.java
@@ -0,0 +1,14 @@
+package com.example.proyectodonpepe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pantallaInicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_inicio);
+    }
+}
