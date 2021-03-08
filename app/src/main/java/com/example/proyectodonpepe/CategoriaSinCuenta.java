22d52392672ae48d997bcc4ad2105d125d1fcd13
LuisM <penaranda-lm@outlook.com>
2021-03-07 19:59:51 -0500

    Subiendo pantalla de Categoria Sin

---
+++ app/src/main/java/com/example/proyectodonpepe/CategoriaSinCuenta.java
@@ -0,0 +1,14 @@
+package com.example.proyectodonpepe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategoriaSinCuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categoria_sin_cuenta);
+    }
+}
